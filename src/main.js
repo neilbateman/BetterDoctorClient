503e33253e5039bd9fa9cb8afe49a4347caddc56
Neil Bateman <neilbatman@gmail.com>
2019-06-11 19:48:12 -0700

    update main.js, issue with .env

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ import $ from 'jquery';
     let promise = doctor.getDrByIssue(name, issue);
     promise.then(function(response) {
       let body = JSON.parse(response);
-      // if(body.data.length === 0) {
-      //   $("#result").append(`<p>No results for the given issue</p>`);
-      // } else {
+      if(body.data.length === 0) {
+        $("#result").append(`<p>No results for the given issue</p>`);
+      } else {
           body.data.forEach(function(dr) {
             $("#result").append(`<p>${dr.profile.first_name} ${dr.profile.last_name}</p><br><p>${dr.practices[0].phones[0].number}</p><br>
         <p>  Website: ${dr.profile.website}</p>`);
@@ -20,6 +20,6 @@ import $ from 'jquery';
         $("#result").text(`<p>Error processing your request: ${error.message}`);
       }
     }
-    );
+  });
   });
 });
