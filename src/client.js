e5bff0325707ad8abb442b211aad51839e8dbbf3
Neil Bateman <neilbatman@gmail.com>
2019-06-15 17:29:17 -0700

    fix request url, properly displaying dr name and phone number

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ export class DoctorClient {
   getDrByIssue(name, issue) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `http://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}location=or-portland,10&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url =  `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.exports.apiKey}&${name}=${issue}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
