5a7abd09b2b1b5c38a9635ddf714e2c9f0cb17aa
Neil Bateman <neilbatman@gmail.com>
2019-06-15 18:10:10 -0700

    add css file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { DoctorClient } from './client.js';
 import $ from 'jquery';
+import './styles.css';
+import 'bootstrap';
 
  $(document).ready(function(){
   $("#search").submit(function(event) {
@@ -10,11 +12,12 @@ import $ from 'jquery';
     let promise = doctor.getDrByIssue(name, issue);
     promise.then(function(response) {
       let body = JSON.parse(response);
+      $("#result").empty();
       if(body.data.length === 0) {
         $("#result").append(`<p>No results for the given issue</p>`);
       } else {
           body.data.forEach(function(dr) {
-            $("#result").append(`<p>${dr.profile.first_name} ${dr.profile.last_name} | ${dr.practices[0].phones[0].number} | Website: ${dr.profile.website}</p>`);
+            $("#result").append(`<p>${dr.profile.first_name} ${dr.profile.last_name} | Phone: ${dr.practices[0].phones[0].number} | Website: ${dr.profile.website}</p>`);
       }), function (error) {
         $("#result").text(`<p>Error processing your request: ${error.message}`);
       }
