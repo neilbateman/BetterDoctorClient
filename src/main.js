9421894d55806f404ce9d30adfa6130360b3224a
Neil Bateman <neilbatman@gmail.com>
2019-06-15 17:39:02 -0700

    improve output information

--- src/main.js
+++ src/main.js
@@ -14,8 +14,7 @@ import $ from 'jquery';
         $("#result").append(`<p>No results for the given issue</p>`);
       } else {
           body.data.forEach(function(dr) {
-            $("#result").append(`<p>${dr.profile.first_name} ${dr.profile.last_name}</p><br><p>${dr.practices[0].phones[0].number}</p><br>
-        <p>  Website: ${dr.profile.website}</p>`);
+            $("#result").append(`<p>${dr.profile.first_name} ${dr.profile.last_name} | ${dr.practices[0].phones[0].number} | Website: ${dr.profile.website}</p>`);
       }), function (error) {
         $("#result").text(`<p>Error processing your request: ${error.message}`);
       }
