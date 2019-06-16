14bd4394cbd196862a542dd20e90c5c9152e8a2a
neilbateman <46337141+neilbateman@users.noreply.github.com>
2019-06-16 12:57:23 -0700

    refactor main.js to only display dr that accepts new patients

--- src/main.js
+++ src/main.js
@@ -22,20 +22,18 @@ import 'bootstrap';
         $("#noresult").append(`No results for the given search`);
       } else {
           body.data.forEach(function(dr) {
-            $("#result").append(`${dr.profile.first_name} ${dr.profile.last_name}  <ul class = ${dr.uid}>`)
             dr.practices.forEach(function(patient){
               let acceptPatient = "No";
               if(patient.accepts_new_patients === true) {
                 acceptPatient = "Yes";
-              }
+            $("#result").append(`${dr.profile.first_name} ${dr.profile.last_name}  <ul class = ${dr.uid}>`)
               $(`.${dr.uid}`).text(`Specialty: ${dr.specialties[0].name} | Phone: ${dr.practices[0].phones[0].number} | Accepting Patients: ${acceptPatient}`)
-            })
-      }), function (error) {
+            }
+          }), function (error) {
         $("#result").text(`Error processing your request: ${error.message}`);
       }
-    }
-  });
-  });
- });
-//| Website: ${dr.profile.website}
-//| Phone: ${dr.practices[0].phones[0].number} | Accepting Patients: ${acceptPatient}
+    });
+  }
+})
+})
+})
