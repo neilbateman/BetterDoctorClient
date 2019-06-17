176ee5c71111028ee2f1432f8a1af738ed58a554
neilbateman <46337141+neilbateman@users.noreply.github.com>
2019-06-16 19:33:50 -0700

    update url

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ export class DoctorClient {
   getDrByIssue(name, issue) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url =  `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.exports.apiKey}&${name}=${issue}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&user_location=45.523064%2C%20-122.676483&sort=distance-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
