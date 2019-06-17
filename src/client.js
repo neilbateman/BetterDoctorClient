1e84a523dcb1ce11a564da029025fcf7702d53f9
Neil Bateman <46337141+neilbateman@users.noreply.github.com>
2019-06-17 16:31:37 -0700

    fix url

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ export class DoctorClient {
   getDrByIssue(name, issue) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&user_location=45.523064%2C%20-122.676483&sort=distance-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&user_location=45.523064%2C%20-122.676483&sort=distance-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
